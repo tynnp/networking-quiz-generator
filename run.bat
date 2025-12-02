0bba88e2d4312432c4f684c728aa38cf5860265d
Hoai Anh <hoaianh16022005@gmail.com>
2025-12-02 21:15:09 +0700

    Chỉnh lại run bat

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 
 start "Client" cmd /k "cd /d client && npm run dev"
-start "Server" cmd /k "cd /d server && uvicorn server.main:app --reload --port 8000" 
+start "Server" cmd /k "uvicorn server.main:app --reload --port 8000" 
 
 ::thêm server.
