65b5c69665bbfea7386bcf0f484beb33be1a373d
tynnp <nguyenngocphuty20172@laivung2.edu.vn>
2025-12-16 14:33:09 +0700

    Thay đổi env, dùng MONGO_URI

--- server/database.py
+++ server/database.py
@@ -2,8 +2,11 @@ from pymongo import MongoClient
 from pymongo.database import Database
 from typing import Optional
 import os
+from dotenv import load_dotenv
 
-MONGODB_URL = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
+load_dotenv()
+
+MONGODB_URL = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 DATABASE_NAME = os.getenv("DATABASE_NAME", "networking-quiz")
 
 _client: Optional[MongoClient] = None
