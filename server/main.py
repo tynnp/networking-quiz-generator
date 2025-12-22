15f253d6264e926317a00118a0b0f8fe54286183
tynnp <nguyenngocphuty20172@laivung2.edu.vn>
2025-12-22 13:13:00 +0700

    API ver 2.1.0

--- server/main.py
+++ server/main.py
@@ -115,7 +115,7 @@ tags_metadata = [
 app = FastAPI(
     title="Networking Quiz Generator API",
     description="API cho hệ thống trắc nghiệm môn Mạng Máy Tính",
-    version="2.0.0",
+    version="2.1.0",
     openapi_tags=tags_metadata
 )
 
